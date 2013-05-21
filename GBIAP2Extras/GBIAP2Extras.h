f718d451e38ffe770d782acb4be6b5d72e419632
Luka Mirosevic <luka@goonbee.com>
2013-05-21 15:27:29 +0200

    Initial Commit

---
+++ GBIAP2Extras/GBIAP2Extras.h
@@ -0,0 +1,13 @@
+//
+//  GBIAP2Extras.h
+//  GBIAP2Extras
+//
+//  Created by Luka Mirosevic on 21/05/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GBIAP2Extras : NSObject
+
+@end
---
+++ GBIAP2Extras/GBIAP2Extras.m
@@ -0,0 +1,13 @@
+//
+//  GBIAP2Extras.m
+//  GBIAP2Extras
+//
+//  Created by Luka Mirosevic on 21/05/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import "GBIAP2Extras.h"
+
+@implementation GBIAP2Extras
+
+@end
